895dfc5cd19dd5dd395223ca2c6640835572c0a2
Onkar Vasav <102866144+onkarv7@users.noreply.github.com>
2024-02-27 11:33:47 +0530

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,11 @@
-const router = require("express").Router();
-const User = require("../models/User");
-const bcrypt = require("bcryptjs");
-const Joi = require("joi");
-const JWT = require("jsonwebtoken");
+// const router = require("express").Router();
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcryptjs";
+import JWT from "jsonwebtoken";
+import Joi from "joi";
 
-// post req > registration of model with the schema , save the user/schema use async await
-// use try catch and send res json
-
-// create schema validation with Joi
+const router = express.Router();
 
 const schema = Joi.object({
   username: Joi.string().required().min(4),
@@ -20,40 +18,63 @@ const Loginschema = Joi.object({
   password: Joi.string().required().min(6),
 });
 
-// router.get("/", async (req, res) => {
-//   const users = await User.find();
-//   res.status(200).send(users);
-// });
-
-// delete based on id
-router.delete("/users/:id", async (req, res) => {
-  const deletedUser = await User.findByIdAndDelete(req.params.id);
-  res.status(200).send(deletedUser);
-});
-
+// register user
 router.post("/register", async (req, res) => {
-  // Joi validation
   const { error } = await schema.validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
-
-  // existing email
-  const emailExist = await User.findOne({ email: req.body.email });
-  if (emailExist) return res.send("email alredy registered");
-
-  // hash the password
   const salt = await bcrypt.genSalt(10);
-  const hashedPasssword = await bcrypt.hash(req.body.password, salt);
+  const hashedpassword = await bcrypt.hash(req.body.password, salt);
 
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: hashedPasssword,
+    password: hashedpassword,
   });
   try {
     const savedUser = await user.save();
-    res.send({ user: user._id });
+    res.status(201).send(savedUser);
   } catch (err) {
-    res.status(400).send({ message: err });
+    res.status(500).send({ message: err });
+  }
+});
+
+// get specific user
+router.get("/user/:id", async (req, res) => {
+  const user = await User.findOne({ _id: req.params.id });
+  res.json(user);
+});
+// get all user
+router.get("/user", async (req, res) => {
+  const users = await User.find();
+  res.send(users);
+});
+// delete user
+
+router.delete("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.json({ message: "User deleted successfully" });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+});
+
+router.put("/user/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
 });
 
@@ -78,14 +99,16 @@ router.post("/login", async (req, res) => {
       return res.status(401).send("Invalid Password");
     }
 
+    const { password, ...other } = user.toObject();
+
     // Password is valid, you can proceed with your login logic
 
     const token = JWT.sign({ id: user._id }, process.env.JWT_KEY);
-    res.header("auth-key", token).send(token);
+    res.header("auth-key", token).send(other);
     // res.send("Login successful");
   } catch (error) {
     res.status(500).send({ message: "Internal Server Error" });
   }
 });
 
-module.exports = router;
+export default router;
